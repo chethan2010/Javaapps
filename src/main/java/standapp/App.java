c72a9adf3bd88f069117da4ed136f3b0aedc9079
Hemanth <hemanthkumarsaketi@gmail.com>
2023-12-27 10:23:48 +0530

    javastandalonecommits

---
+++ src/main/java/standapp/App.java
@@ -0,0 +1,13 @@
+package standapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
